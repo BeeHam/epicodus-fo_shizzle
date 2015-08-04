ec5e2fcd22a32bc70d30ef5920761a6629a35353
Terry Marr & Brigham Thompson <student@epicodus.com>
2015-08-03 17:03:24 -0700

    a work in progress

--- spec/foshizzle_spec.rb
+++ spec/foshizzle_spec.rb
@@ -5,4 +5,8 @@ describe('String#foshizzle') do
   it("replaces every 'S' with a 'Z'") do
     expect("roses".foshizzle).to(eq("rozez"))
   end
+
+  it("does not change first letter of sentence to z") do
+    expect("sunshine".foshizzle).to(eq("sunzhine"))
+  end
 end
